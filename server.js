eeab98dbd9ebb70ee0b7af9b5691286f7d33525c
KAUSHAL KUMAR <kaushal80320252@gmail.com>
2026-04-17 13:14:22 +0530

    update code

--- server.js
+++ server.js
@@ -40,7 +40,7 @@
 //     const uID = parseInt(request.params.id);
 //     const { name, address } = request.body;
 //     const uIndex = users.findIndex(u => u.id === uID);
-//     if (uIndex === -1) {
+//     if (uIndex === -1) {    
 //         response.json({
 //             message: "User Not Found"
 //         })
@@ -63,61 +63,95 @@
 // Q (2) ----------
 
 
-const express = require('express'); 
- const app = express();
+// const express = require('express'); 
+//  const app = express();
+// app.use(express.json());
+
+// const employees = [
+//     { id: 1, name: "Amit", department: "IT" },
+//     { id: 2, name: "Ravi", department: "HR" }
+// ];
+
+// // GET Employees
+// app.get('/employees', (req, res) => {
+//     res.json({
+//         message: "All Employees",
+//         data: employees
+//     });
+// });
+
+// // DELETE Employee
+// app.delete('/deleteEmployee/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+
+//     const index = employees.findIndex(e => e.id === id);
+
+//     if (index === -1) {
+//         return res.json({ message: "Employee Not Found" });
+//     }
+
+//     const deleted = employees.splice(index, 1);
+
+//     res.json({
+//         message: "Employee Deleted",
+//         data: deleted
+//     });
+// });
+
+// // UPDATE Employee
+// app.put('/updateEmployee/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const { name, department } = req.body;
+
+//     const index = employees.findIndex(e => e.id === id);
+
+//     if (index === -1) {
+//         return res.json({ message: "Employee Not Found" });
+//     }
+
+//     employees[index] = { id, name, department };
+
+//     res.json({
+//         message: "Employee Updated"
+//     });
+// });
+
+// app.listen(3300, () => {
+//     console.log("Server is Running.....");
+// })
+
+
+
+
+// Q(3) ---------
+
+const express = require('express');
+const app = express();
 app.use(express.json());
 
-const employees = [
-    { id: 1, name: "Amit", department: "IT" },
-    { id: 2, name: "Ravi", department: "HR" }
-];
+const users = ["ram", "mohan", "shyam"];
 
-// GET Employees
-app.get('/employees', (req, res) => {
-    res.json({
-        message: "All Employees",
-        data: employees
+//  GET METHOD
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        message: "get data successfully",
+        users: users
     });
 });
 
-// DELETE Employee
-app.delete('/deleteEmployee/:id', (req, res) => {
-    const id = parseInt(req.params.id);
+//  POST METHOD
+app.post('/users', (req, res) => {
+    const newUser = req.body.name;
+    users.push(newUser);
 
-    const index = employees.findIndex(e => e.id === id);
-
-    if (index === -1) {
-        return res.json({ message: "Employee Not Found" });
-    }
-
-    const deleted = employees.splice(index, 1);
-
-    res.json({
-        message: "Employee Deleted",
-        data: deleted
+    res.status(201).json({
+        message: "User added successfully",
+        users: users
     });
 });
 
-// UPDATE Employee
-app.put('/updateEmployee/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name, department } = req.body;
-
-    const index = employees.findIndex(e => e.id === id);
-
-    if (index === -1) {
-        return res.json({ message: "Employee Not Found" });
-    }
-
-    employees[index] = { id, name, department };
-
-    res.json({
-        message: "Employee Updated"
-    });
+app.listen(3000, () => {
+    console.log("Server is running on port 3000 🚀");
 });
-
-app.listen(3300, () => {
-    console.log("Server is Running.....");
-})
 
 
